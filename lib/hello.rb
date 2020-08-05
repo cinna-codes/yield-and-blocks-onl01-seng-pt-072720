145a73f470930c85037a29fc444ac6f5ae85b6e1
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-05 01:10:28 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def hello_t(array)
   if block_given?
   i = 0
@@ -13,7 +15,40 @@ end
 # call your method here!
 
 
-#hello_t(["Tim", "Tom", "Jim"]) do |name|
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+# binding.pry
+#  if name.start_with?("T")
+#    puts "Hi, #{name}"
+#  end
+# end
+
+# def make_salad
+#   yield "lettuce"
+#   yield "tomatoes"
+#   yield "croutons"
+# end
+#
+# make_salad { |ingredient| puts ingredient }
+#
+# newarr = [1, 2, 3]
+#
+# test1 = []
+#
+# newarr.each do |num|
+#   test1 << num
+# end
+#
+# test1 << 4
+#
+# print test1
+
+
+#puts [test1]
+
+# newarr.each { |num| puts num+1 }
+
+#["Tim", "Tom", "Jim"].each do |name|
+#  binding.pry
 #  if name.start_with?("T")
 #    puts "Hi, #{name}"
 #  end
